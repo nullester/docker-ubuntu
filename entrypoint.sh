fadd99f9e54cbe84a4facbc623a2eeb557a9cede
nullester <65467511+nullester@users.noreply.github.com>
2022-12-05 17:12:09 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ echo && echo -e "\033[032mI AM \033[035m$( whoami )\033[032m!\033[0m" && echo
 su root -c "export HOME=/root"
 su docker -c "export HOME=/home/docker"
 
-echo -e -n "Server \033[032m${HOSTNAME}\033[0m up and running!"
+echo -e "Server \033[032m${HOSTNAME}\033[0m up and running!"
 tail -f /dev/null
